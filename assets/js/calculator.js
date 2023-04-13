7a95b4dff157d501e75c3779b383806a182906f3
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-04-13 16:36:35 -0300

    Cria a classe utilitária calculator

---
+++ assets/js/calculator.js
@@ -0,0 +1,23 @@
+class Calculator {
+
+    static getHeightString(height) {
+        const meterHeight = height / 10;
+        const feetHeight = height / 3.048;
+        const englishSystemFeetPart = Math.floor(feetHeight);
+        const englishSystemInchPart = 12 * (feetHeight - englishSystemFeetPart);
+
+        return `${englishSystemFeetPart}'${englishSystemInchPart.toFixed(1)}" (${meterHeight.toFixed(2)} m)`;
+    }
+
+    static getWeightString(weight) {
+        const weightLbs = weight * 0.220462;
+        const weightKgs = weight / 10;
+        return `${(weightLbs).toFixed(1)} lbs (${(weightKgs).toFixed(1)} kg)`;
+    }
+
+    static getGenderRate(rate) {
+        const chanceToBeFemale = rate/8*100;
+        const chanceToBeMale = 100 - chanceToBeFemale;
+        return [chanceToBeMale, chanceToBeFemale];
+    }
+}
