f3fd6fc81ae03ac24a8e4e60ad15f06c7653bc30
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-04-13 16:37:54 -0300

    Add mais dados com novas requisições assícronas

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,26 +1,57 @@
 
 const pokeApi = {};
 
-function convertPokeApiDetailsToPokemon(pokeDetails) {
+async function convertPokeApiDetailsToPokemon(pokeDetails) {
     const pokemonTypes = pokeDetails.types.map(slot => slot.type.name);
     const [pokemonMainType] = pokemonTypes;
     const pokemonPic = pokeDetails.sprites.other['official-artwork'].front_default;
-    
-    return new Pokemon(pokeDetails.name, pokeDetails.id, pokemonTypes, pokemonMainType, pokemonPic)
+    const pokemonAbilities = pokeDetails.abilities.map(a => a.ability.name);
+    const pokemonStats = pokeDetails.stats.map(s => new PokemonStat(s.stat.name, s['base_stat']));
+    const pokemonMoves = pokeDetails.moves.map(m => m.move.name);
+
+    const speciesInfos = await pokeApi.getSpeciesInfo(pokeDetails.species.url);
+
+    const apiEvolutionChain = await pokeApi.getEvolutionChain(speciesInfos['evolution_chain'].url);    
+    const evolutionElement = pokeApi.convertApiEvChainToEvElement(apiEvolutionChain.chain);
+
+    return new Pokemon(
+        pokeDetails.name, pokeDetails.id, pokemonTypes, pokemonPic, pokeDetails.height,
+        pokeDetails.weight, pokemonAbilities, pokemonStats, pokemonMoves,
+        speciesInfos.habitat.name, speciesInfos['gender_rate'],
+        speciesInfos['egg_groups'].map(eg => eg.name), speciesInfos['is_baby'],
+        speciesInfos['is_legendary'], speciesInfos['is_mythical'], evolutionElement
+    );
 }
 
-pokeApi.getPokemon = (pokemonEntry) => {
+pokeApi.convertApiEvChainToEvElement = (chain) => {
+    if(chain['evolves_to'].length === 0) return new EvolutionElement(chain.species.name, []);
+    return new EvolutionElement(
+        chain.species.name,
+        chain['evolves_to'].map(el => pokeApi.convertApiEvChainToEvElement(el))
+    );
+}
+
+pokeApi.getSpeciesInfo = async (url) => {
+    return fetch(url).then(r => r.json()).catch(console.log);
+}
+
+pokeApi.getEvolutionChain = async (url) => {
+    return fetch(url).then(r => r.json()).catch(console.log);
+}
+
+pokeApi.getPokemon = async (pokemonEntry) => {
     return fetch(pokemonEntry.url)
         .then(response => response.json())
-        .then(convertPokeApiDetailsToPokemon);
+        .then(convertPokeApiDetailsToPokemon)
+        .catch(console.log);
 }
 
-pokeApi.getPokemons = (offset, limit = 10) => {
+pokeApi.getPokemons = async (offset, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then(response => response.json())
         .then(body => body.results)
         .then(pokemonEntrys => pokemonEntrys.map(pokeApi.getPokemon))
         .then(detailRequests => Promise.all(detailRequests))
-        .catch(err => console.log(err));
+        .catch(console.log);
 }
